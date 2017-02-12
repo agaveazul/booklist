b677a50db856e9831eb965cd2f95d38f536109d2
Richard Strauss <rstrauss91@gmail.com>
2017-02-12 17:34:08 -0500

    Add CSV and JSON requests

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,6 +7,13 @@ def index
 
 respond_to do |format|
     format.html
+    format.text
+    format.csv do
+      render plain: Book.generate_csv(@books)
+    end
+    format.json do
+    render json: @books
+  end
 end
 end
 
